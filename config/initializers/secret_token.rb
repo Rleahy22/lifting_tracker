97d3e1f157472576bc97d7ca1c0616bbf0f000b9
Ryan Leahy <ryanleahy90@gmail.com>
2013-09-25 15:30:57 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LiftingTracker::Application.config.secret_token = '401e4d48f07479f67d35875a26d2708612207ae22093e5e97d9f5f8bc87e055aff3435a576171fba3adb58db87851bab90289d6879cb0a2375dfa4c77357774b'
